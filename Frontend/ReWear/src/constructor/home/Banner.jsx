00fcb89200fac6b44562ef6f982ebd84cc750e98
shahamit20 <shahamits123@gmail.com>
2025-07-12 11:49:57 +0530

    change

---
+++ Frontend/ReWear/src/constructor/home/Banner.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Banner() {
+  return (
+    <>
+        <div className='w-full mt-5 h-[25rem] border border-red-500'>
+           
+        </div>
+    </>
+  )
+}
+
+export default Banner
--- Frontend/ReWear/src/constructor/home/Laddingpage.jsx
+++ Frontend/ReWear/src/constructor/home/Laddingpage.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Navbar from '../other/Navbar'
+import Banner from './Banner'
 
 function Laddingpage() {
   return (
    <>
    <Navbar />
-   </> 
+   <div className='w-screen p-2 flex flex-col justify-center items-center'>
+
+   <Banner />
+   </div>
+      </> 
   )
 }
 
