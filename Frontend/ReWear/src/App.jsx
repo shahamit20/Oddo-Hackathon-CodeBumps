964f74984983a84ca22b992600006ebd39b507df
shahamit20 <shahamits123@gmail.com>
2025-07-12 16:36:19 +0530

    connect

--- Frontend/ReWear/src/App.jsx
+++ Frontend/ReWear/src/App.jsx
@@ -5,6 +5,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Laddingpage from './constructor/home/Laddingpage'
 import Dashboard from './dashboard/Dashbord'
+import AddProduct from './constructor/Product/AddProduct';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,6 +16,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Laddingpage />} />
         <Route path="/dashboard" element={<Dashboard />} /> 
+        <Route path="/upload" element={<AddProduct />} /> 
+        {/* <Route path="/dashboard" element={<Dashboard />} />  */}
       </Routes>
     </>
   )
