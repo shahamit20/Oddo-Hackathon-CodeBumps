fb456f0dbb1b8b3f847175b702912f3ae4219c2d
shahamit20 <shahamits123@gmail.com>
2025-07-12 12:39:01 +0530

    category

---
+++ Frontend/ReWear/src/constructor/home/Category.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Category() {
+  return (
+    <div className='w-full mt-5 h-[25rem] bg-orange-50 rounded-xl'>
+
+    <h1 className='text-ce'>Shop by Category</h1>
+    </div>
+  )
+}
+
+export default Category
